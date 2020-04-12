bb73df5ea207f7f3cd7509d648037f14ca406c7f
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-12 16:55:56 -0300

    Other entities and second migration

--- PROJETOFINAL_SalesWeb/Data/PROJETOFINAL_SalesWebContext.cs
+++ PROJETOFINAL_SalesWeb/Data/PROJETOFINAL_SalesWebContext.cs
@@ -14,6 +14,8 @@ namespace PROJETOFINAL_SalesWeb.Data
         {
         }
 
-        public DbSet<PROJETOFINAL_SalesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
