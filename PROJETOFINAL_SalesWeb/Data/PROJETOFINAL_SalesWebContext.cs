1b51d50a8b9b1809f32691512146d00a71dd730d
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-12 14:58:55 -0300

    CRUD Scaffolding

---
+++ PROJETOFINAL_SalesWeb/Data/PROJETOFINAL_SalesWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PROJETOFINAL_SalesWeb.Models;
+
+namespace PROJETOFINAL_SalesWeb.Data
+{
+    public class PROJETOFINAL_SalesWebContext : DbContext
+    {
+        public PROJETOFINAL_SalesWebContext (DbContextOptions<PROJETOFINAL_SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PROJETOFINAL_SalesWeb.Models.Department> Department { get; set; }
+    }
+}
