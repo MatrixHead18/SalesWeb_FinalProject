c65da46b590630d0c5ed7f9298e48c46fa7c8869
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-13 14:07:17 -0300

    Seeding Service

--- PROJETOFINAL_SalesWeb/Startup.cs
+++ PROJETOFINAL_SalesWeb/Startup.cs
@@ -39,14 +39,16 @@ namespace PROJETOFINAL_SalesWeb
             services.AddDbContext<PROJETOFINAL_SalesWebContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("PROJETOFINAL_SalesWebContext"), 
                     builder => builder.MigrationsAssembly("PROJETOFINAL_SalesWeb")));
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
