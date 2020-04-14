8811eaf7809606c2874069f26c22981fbaf41a92
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-14 14:34:48 -0300

    Foreign Key not null

--- PROJETOFINAL_SalesWeb/Migrations/20200414173026_DepartmentForeignKey.Designer.cs
+++ PROJETOFINAL_SalesWeb/Migrations/20200414173026_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PROJETOFINAL_SalesWeb.Data;
 
 namespace PROJETOFINAL_SalesWeb.Migrations
 {
     [DbContext(typeof(PROJETOFINAL_SalesWebContext))]
-    partial class PROJETOFINAL_SalesWebContextModelSnapshot : ModelSnapshot
+    [Migration("20200414173026_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
