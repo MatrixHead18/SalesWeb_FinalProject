f2684e1ae8886c5092129f25a80085e34873c1f1
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-14 14:19:49 -0300

    Simple Create Form

--- PROJETOFINAL_SalesWeb/Controllers/SellersController.cs
+++ PROJETOFINAL_SalesWeb/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PROJETOFINAL_SalesWeb.Models;
 using PROJETOFINAL_SalesWeb.Services;
 
 namespace PROJETOFINAL_SalesWeb.Controllers
@@ -20,6 +21,19 @@ namespace PROJETOFINAL_SalesWeb.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
