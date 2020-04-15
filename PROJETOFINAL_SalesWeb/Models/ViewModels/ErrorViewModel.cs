6db8b8e3d59bfe861db7c01add26540c00b2ca97
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-14 22:58:18 -0300

    returning error custom page

--- PROJETOFINAL_SalesWeb/Models/ViewModels/ErrorViewModel.cs
+++ PROJETOFINAL_SalesWeb/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace PROJETOFINAL_SalesWeb.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
