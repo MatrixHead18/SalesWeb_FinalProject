249a8db685fc62ae379aef58e256c0fa73318ef2
Guilherme Augusto <guilherme.augusto04@hotmail.com.br>
2020-04-14 22:42:35 -0300

    Update seller and custom service exception

--- PROJETOFINAL_SalesWeb/Controllers/SellersController.cs
+++ PROJETOFINAL_SalesWeb/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using PROJETOFINAL_SalesWeb.Models;
 using PROJETOFINAL_SalesWeb.Models.ViewModels;
 using PROJETOFINAL_SalesWeb.Services;
+using PROJETOFINAL_SalesWeb.Services.Exceptions;
 
 namespace PROJETOFINAL_SalesWeb.Controllers
 {
@@ -75,6 +76,45 @@ namespace PROJETOFINAL_SalesWeb.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ PROJETOFINAL_SalesWeb/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROJETOFINAL_SalesWeb.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
